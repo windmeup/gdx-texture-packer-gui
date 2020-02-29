d8da7dae42a3158e9b6d7a1b088b296a3bb2bb66
wangdade <babamiya224@gmail.com>
2020-02-29 14:33:17 +0800

    only modify preview data

--- core/src/com/crashinvaders/texturepackergui/controllers/SkeletonController.java
+++ core/src/com/crashinvaders/texturepackergui/controllers/SkeletonController.java
@@ -1,87 +1,61 @@
 package com.crashinvaders.texturepackergui.controllers;
 
 import com.crashinvaders.texturepackergui.controllers.main.MainController;
-import com.crashinvaders.texturepackergui.utils.JacksonUtils;
 import com.crashinvaders.texturepackergui.views.canvas.widgets.spine.AnimationPanel;
 import com.esotericsoftware.spine.SkeletonData;
 import com.github.czyzby.autumn.annotation.Component;
 import com.github.czyzby.autumn.annotation.Inject;
 import lombok.Setter;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Paths;
-
 @Component
 public class SkeletonController {
 
-  private File skeletonFile;
-
   @Setter
   private SkeletonData skeletonData;
-
-  private com.badlogic.gdx.tools.spine.data.SkeletonData data;
 
   @Inject
   MainController mainController;
 
   public void clear() {
-    skeletonFile = null;
     skeletonData = null;
-    data = null;
   }
 
-  public void setSkeletonPath(String skeletonPath) throws IOException {
-    skeletonFile = Paths.get(skeletonPath).toFile();
-    data = JacksonUtils.readValue(
-        skeletonFile, com.badlogic.gdx.tools.spine.data.SkeletonData.class
-    );
-    if (data == null || data.getSkeleton() == null || data.getAnimations() == null || data.getBones() == null ||
-        data.getSkins() == null || data.getSlots() == null) {
-      clear();
-      throw new IOException("not skeleton json");
-    }
-  }
-
-  public void setSkeletonX(int skeletonX) throws IOException {
-    if (skeletonData != null) {
+  public void setSkeletonX(int skeletonX) {
+    if (skeletonData != null && skeletonData.getX() != skeletonX) {
       skeletonData.setX(skeletonX);
-      modified(data.getSkeleton().setX(skeletonX));
+      modified();
     }
   }
 
-  public void setSkeletonY(int skeletonY) throws IOException {
-    if (skeletonData != null) {
+  public void setSkeletonY(int skeletonY) {
+    if (skeletonData != null && skeletonData.getY() != skeletonY) {
       skeletonData.setY(skeletonY);
-      modified(data.getSkeleton().setY(skeletonY));
+      modified();
     }
   }
 
-  public void setSkeletonWidth(int skeletonWidth) throws IOException {
-    if (skeletonData != null) {
+  public void setSkeletonWidth(int skeletonWidth) {
+    if (skeletonData != null && skeletonData.getWidth() != skeletonWidth) {
       skeletonData.setWidth(skeletonWidth);
-      modified(data.getSkeleton().setWidth(skeletonWidth));
+      modified();
     }
   }
 
-  public void setSkeletonHeight(int skeletonHeight) throws IOException {
-    if (skeletonData != null) {
+  public void setSkeletonHeight(int skeletonHeight) {
+    if (skeletonData != null && skeletonData.getHeight() != skeletonHeight) {
       skeletonData.setHeight(skeletonHeight);
-      modified(data.getSkeleton().setHeight(skeletonHeight));
+      modified();
     }
   }
 
   public void moveSelected(int offsetX, int offsetY) {
     if (skeletonData != null) {
-      // TODO delete data
+      // TODO
     }
   }
 
-  private void modified(boolean dataChanged) throws IOException {
-    if (dataChanged) {
-      getAnimationPanel().relayout();
-      JacksonUtils.writeValue(skeletonFile, data);
-    }
+  private void modified() {
+    getAnimationPanel().relayout();
   }
 
   private AnimationPanel getAnimationPanel() {
